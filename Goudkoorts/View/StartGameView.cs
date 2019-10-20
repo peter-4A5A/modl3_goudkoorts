493ece96fc7eb1ddb80cd1d2a624cdc35466dc88
Peter Romijn <peter@samebestdevelopment.nl>
2019-10-20 22:12:30 +0200

    Completed start view

--- Goudkoorts/View/StartGameView.cs
+++ Goudkoorts/View/StartGameView.cs
@@ -19,6 +19,13 @@ namespace Goudkoorts.View
             Console.WriteLine("|  Je kan een wissel verwisselen        |");
             Console.WriteLine("|  Dit gaat met verschillende characters|");
             Console.WriteLine("|  Namelijk die je ziet in het voorbeeld|");
+            Console.WriteLine("|  De warehouses zijn: A,B en C         |");
+            Console.WriteLine("|  De wissels zijn te bedienen me:      |");
+            Console.WriteLine("|  W, R, E, T, Q                        |");
+            Console.WriteLine("|  De x is het einde                    |");
+            Console.WriteLine("|  k is het dock en een S staat         |");
+            Console.WriteLine("|  Voor het schip                       |");
+            Console.WriteLine("|  De map ziet er als het volgende uit  |");
             Console.WriteLine("x--------k-|");
             Console.WriteLine("           |");
             Console.WriteLine("A--| |---- |");
